f3348b2425769ffe433cd209282c61818857fb5b
ctadev <53548168+ctadev@users.noreply.github.com>
2022-07-17 01:59:08 -0400

    fixed cart responsive

--- components/Category/Collections.jsx
+++ components/Category/Collections.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import s from "./collections.module.scss";
 import { urlFor } from "../../lib/client";
 import Link from "next/link";
 import StarIcon from "@mui/icons-material/Star";
 import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import ReactPaginate from "react-paginate";
 
 const Collections = ({ category, title }) => {
+  const [products, setProducts] = useState(category);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const showProducts = 15;
+  const paginateProducts = pageNumber * showProducts;
+
+  const pageCount = Math.ceil(products.length / showProducts);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  const displayProducts = products
+    .slice(paginateProducts, paginateProducts + showProducts)
+    .map((product) => {});
+
   var formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -53,63 +70,85 @@ const Collections = ({ category, title }) => {
         </section>
 
         <section className={s.products_container}>
-          {category?.map((categories) => (
-            <Link
-              href={`/category/${title}/${categories.slug.current}`}
-              key={categories._id}
-            >
-              <div className={s.product}>
-                <div className={s.image_container}>
-                  <Image
-                    src={urlFor(categories.mainImage[0]).url()}
-                    alt=""
-                    layout="fill"
-                    objectFit="contain"
-                    className={s.image}
-                  />
-                </div>
-                <h4>{categories.name}</h4>
-                {categories.ratings && (
-                  <div className={s.rating}>
-                    <div className={s.star_container}>
-                      <div className={s.outer_star}>
-                        <StarOutlineIcon className={s.icon} />
-                        <StarOutlineIcon className={s.icon} />
-                        <StarOutlineIcon className={s.icon} />
-                        <StarOutlineIcon className={s.icon} />
-                        <StarOutlineIcon className={s.icon} />
-                        <div
-                          className={s.inner_star}
-                          style={{ width: `${stars.star(categories.ratings)}` }}
-                        >
-                          <StarIcon className={s.icon} />
-                          <StarIcon className={s.icon} />
-                          <StarIcon className={s.icon} />
-                          <StarIcon className={s.icon} />
-                          <StarIcon className={s.icon} />
+          {products
+            .slice(paginateProducts, paginateProducts + showProducts)
+            ?.map((categories) => (
+              <Link
+                href={`/category/${title}/${categories.slug.current}`}
+                key={categories._id}
+              >
+                <div className={s.product}>
+                  <div className={s.image_container}>
+                    <Image
+                      src={urlFor(categories.mainImage[0]).url()}
+                      alt=""
+                      layout="fill"
+                      objectFit="contain"
+                      className={s.image}
+                    />
+                  </div>
+                  <h4>{categories.name}</h4>
+                  {categories.ratings && (
+                    <div className={s.rating}>
+                      <div className={s.star_container}>
+                        <div className={s.outer_star}>
+                          <StarOutlineIcon className={s.icon} />
+                          <StarOutlineIcon className={s.icon} />
+                          <StarOutlineIcon className={s.icon} />
+                          <StarOutlineIcon className={s.icon} />
+                          <StarOutlineIcon className={s.icon} />
+                          <div
+                            className={s.inner_star}
+                            style={{
+                              width: `${stars.star(categories.ratings)}`,
+                            }}
+                          >
+                            <StarIcon className={s.icon} />
+                            <StarIcon className={s.icon} />
+                            <StarIcon className={s.icon} />
+                            <StarIcon className={s.icon} />
+                            <StarIcon className={s.icon} />
+                          </div>
                         </div>
                       </div>
+                      <p className={s.reviews_text}>
+                        &nbsp;{categories.reviews} reviews
+                      </p>
                     </div>
-                    <p className={s.reviews_text}>&nbsp;{categories.reviews} reviews</p>
-                  </div>
-                )}
-                {categories.sales ? (
-                  <div className={s.price_container}>
-                    <p className={s.strike_through}>
-                      {formatter.format(categories.price)}
-                    </p>
-                    <p>{formatter.format(categories.sales)}</p>
-                  </div>
-                ) : (
-                  <div className={s.price_container}>
-                    <p>{formatter.format(categories.price)}</p>
-                  </div>
-                )}
-                <h5>View</h5>
-                {categories.sales && <div className={s.sales}>Sale</div>}
-              </div>
-            </Link>
-          ))}
+                  )}
+                  {categories.sales ? (
+                    <div className={s.price_container}>
+                      <p className={s.strike_through}>
+                        {formatter.format(categories.price)}
+                      </p>
+                      <p>{formatter.format(categories.sales)}</p>
+                    </div>
+                  ) : (
+                    <div className={s.price_container}>
+                      <p>{formatter.format(categories.price)}</p>
+                    </div>
+                  )}
+                  <h5>View</h5>
+                  {categories.sales && <div className={s.sales}>Sale</div>}
+                </div>
+              </Link>
+            ))}
+        </section>
+
+        <section className={s.pagination}>
+          {products.length > 15 && (
+            <ReactPaginate
+              previousLabel={"Previous"}
+              nextLabel={"Next"}
+              pageCount={pageCount}
+              onPageChange={changePage}
+              containerClassName={s.paginationBtns}
+              previousClassName={s.previousBtn}
+              nextLinkClassName={s.nextBtn}
+              disabledClassName={s.paginationDisabled}
+              activeClassName={s.paginationActive}
+            />
+          )}
         </section>
       </div>
     </main>
